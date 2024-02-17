74a889b01a8a89b82d2fb20aa88c9f538d044ca5
Eleonora Trikidou <trikidou@csd.auth.gr>
2024-02-17 20:12:46 +0200

    Add files via upload

---
+++ JS-Easy/javascript00.js
@@ -0,0 +1,29 @@
+/*let myString='Hello World!';
+console.log(myString);
+*/
+
+/*let s1='Hello';
+let s2='World!';
+let s=s1+' '+s2;
+console.log(s);
+*/
+
+/*let s='My dog is the best';
+console.log(s[0],s[s.length-4],typeof s);
+*/
+
+/*let firstName='Eleonora';
+let lastName='Trikidou';
+console.log(`Hello ${firstName+' '+lastName+'!'}`);
+*/
+
+/*let dayOfTheWeek='Saturday';
+let day=17;
+let month='February';
+let year=2024;
+console.log(`${dayOfTheWeek+','+day+' '+month+' '+year}`);
+*/
+
+let firstName='Eleonora';
+let surName='Trikidou';
+console.log(`${'Hello'+' '+firstName+' '+surName+'!'}`);
