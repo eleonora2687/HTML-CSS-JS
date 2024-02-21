d11320ec726658935fcc02a8a57fa3820a97bfa5
Eleonora Trikidou <trikidou@csd.auth.gr>
2024-02-21 13:42:29 +0200

    Add files via upload

---
+++ JS-Easy/javascript03.js
@@ -0,0 +1,114 @@
+'use strict'
+
+/*
+let nameA='Manos';
+let nameB='Nora';
+
+let dieA, dieB;
+
+dieA=Math.floor((Math.random() * 10)%6+1);
+dieB=Math.floor((Math.random() * 10)%6+1);
+
+console.log(dieA);
+console.log(dieB);
+
+
+if(dieA > dieB)
+    console.log('The winner is Manos');
+else if(dieA < dieB)
+    console.log('The winner is Nora');
+else
+    console.log('Draw');
+
+
+
+
+let x=4;
+let sign=(x<-3 && x>-5?-1:x==0?0:x>1 && x<5?12:5);
+console.log(sign);
+
+
+
+
+let x=5;
+let y;
+
+switch(x)
+{
+    case 1:
+        y='hello';
+        break;
+    case 2:
+        y='yes';
+        break;
+    case 3:
+        y='Nora';
+        break;
+    default:
+        y=50;
+        break;
+}
+
+console.log(y);
+
+
+
+let x = 8;
+let myString;
+
+switch (x) {
+    case 0:
+        
+    case 1:
+        
+    case 2:
+        
+    case 3:
+        
+    case 4:
+        
+    case 5:
+        myString='less than or equal to 5'
+        break;
+    
+    case 6:
+        
+    case 7:
+        '
+    case 8:
+        
+    case 9:
+        
+    case 10:
+        myString='greater than 5 and less than or equal to 10'
+        break;
+                                                              
+    default:
+        myString='invalid value'
+        break;
+}
+console.log(myString);
+
+
+
+let hours=13;
+let minutes=52;
+let seconds=48;
+
+if(hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60 && seconds >=0 && seconds < 60)
+    if (hours<10) {
+        console.log('0' + hours + ':' + minutes + ':' + second);
+    }
+    else
+    {
+        console.log(hours + ':' + minutes + ':' + seconds);
+}
+*/
+
+let x=5;
+
+{
+    let y=3;
+}
+
+console.log(y); //error
