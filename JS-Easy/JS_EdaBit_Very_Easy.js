c9f88f379fd92dcc46cd2ec7934023e6458483f7
Eleonora Trikidou <trikidou@csd.auth.gr>
2024-04-15 09:08:25 +0300

    Add files via upload

---
+++ JS-Easy/JS_EdaBit_Very_Easy.js
@@ -0,0 +1,132 @@
+'use strict'
+
+// How Edabit Works
+
+/* function hello() {
+	return "hello edabit.com"
+}
+
+console.log(hello()); */
+
+
+// Return the Sum of Two Numbers
+
+/* function addition(a, b) {
+        return a+b;
+}
+
+console.log(addition(3, 2));
+console.log(addition(-3, -6));
+console.log(addition(7, 3)); */
+
+// Convert Minutes into Seconds
+
+/* function convert(minutes) {
+        return minutes*60;
+}
+
+console.log(convert(5));
+console.log(convert(3));
+console.log(convert(2)); */
+
+// Return the Next Number from the Integer Passed
+
+/* function addition(num) {
+        return num+1;
+}
+
+console.log(addition(0));
+console.log(addition(9));
+console.log(addition(-3)); */
+
+// Area of a Triangle
+
+/* function triArea(base, height) {
+        return 1/2*(base)*(height);
+}
+
+console.log(triArea(3, 2));
+console.log(triArea(7, 4));
+console.log(triArea(10, 10)); */
+
+// Convert Age to Days
+
+/* function calcAge(age) {
+        return age*365;
+}
+
+console.log(calcAge(65));
+console.log(calcAge(0));
+console.log(calcAge(20)); */
+
+// Return the First Element in an Array
+
+/* function getFirstValue(arr) {
+        return arr[0];
+}
+
+console.log(getFirstValue([1, 2, 3]));
+console.log(getFirstValue([80, 5, 100]));
+console.log(getFirstValue([-500, 0, 50])); */
+
+
+// Power Calculator
+
+/* function circuitPower(voltage, current) {
+        return voltage*current;
+}
+
+console.log(circuitPower(230, 10));
+console.log(circuitPower(110, 3));
+console.log(circuitPower(480, 20)); */
+
+// Convert Hours into Seconds
+
+/* function howManySeconds(hours) {
+        return hours*3600;
+}
+
+console.log(howManySeconds(2));
+console.log(howManySeconds(10));
+console.log(howManySeconds(24)); */
+
+// Maximum Edge of a Triangle
+
+/* function nextEdge(side1, side2) {
+        return (side1+side2)-1;
+}
+
+console.log(nextEdge(8, 10));
+console.log(nextEdge(5, 7));
+console.log(nextEdge(9, 2)); */
+
+// Return the Remainder from Two Numbers
+
+/* function remainder(x, y) {
+        return x%y;
+}
+
+console.log(remainder(1, 3));
+console.log(remainder(3, 4));
+console.log(remainder(-9, 45));
+console.log(remainder(5, 5)); */
+
+// Find the Perimeter of a Rectangle
+
+/* function findPerimeter(length, width) {
+        return 2*(length+width);
+}
+
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9)); */
+
+// Return Something to Me!
+
+function giveMeSomething(a) {
+    return "something" +" "+a;
+}
+
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("Bob Jane"));
+console.log(giveMeSomething("something"));
